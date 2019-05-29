f7fc0f01d8f53afecf098e99ff78cdd4543f027d
rafauu <gliniakr@gmail.com>
2019-05-29 17:17:39 +0200

    first implementation + ut

--- IdAllocator.hpp
+++ IdAllocator.hpp
@@ -8,14 +8,53 @@ public:
     IdAllocator(Size size) :
         idHolder(size)
     {}
-    void method();
+    auto allocate();
+    void deallocate();
+    void reset();
 private:
+    void resizeIfNeeded();
     boost::dynamic_bitset<> idHolder;
 };
 
+
 template <typename Size>
-void IdAllocator<Size>::method()
+auto IdAllocator<Size>::allocate()
 {
-    ;
+    resizeIfNeeded();
+    auto allocatedId{idHolder.count()};
+    idHolder.set(allocatedId);
+    std::cout << "Id " << allocatedId << " has been allocated" << std::endl;
+    return allocatedId;
 }
 
+template <typename Size>
+void IdAllocator<Size>::deallocate()
+{
+    if (idHolder.count() > 0)
+    {
+        idHolder >>= 1;
+        std::cout << "Id " << idHolder.count() << " has been deallocated" << std::endl;
+    }
+    else
+    {
+        std::cout << "Nothing to deallocate" << std::endl;
+    }
+}
+
+template <typename Size>
+void IdAllocator<Size>::resizeIfNeeded()
+{
+    if (idHolder.all())
+    {
+        std::cout << "Container has been resized from: " << idHolder.size();
+        idHolder.resize(2 * idHolder.size());
+        std::cout << " to: " << idHolder.size() << std::endl;
+    }
+}
+
+template <typename Size>
+void IdAllocator<Size>::reset()
+{
+    std::cout << "Container will be cleaned" << std::endl;
+    idHolder.reset();
+}
--- IdAllocatorTestSuite.cpp
+++ IdAllocatorTestSuite.cpp
@@ -3,7 +3,7 @@
 
 namespace
 {
-constexpr auto DEFAULT_ALLOCATOR_SIZE = 8u;
+constexpr auto DEFAULT_ALLOCATOR_SIZE = 3u;
 }
 
 class IdAllocatorTestSuite : public ::testing::Test
@@ -12,9 +12,53 @@ protected:
     IdAllocator<unsigned> idAllocator{DEFAULT_ALLOCATOR_SIZE};
 };
 
-TEST_F(IdAllocatorTestSuite, exampleTest)
+TEST_F(IdAllocatorTestSuite, shallAllocateMemoryAndIdWhenRanOutOfBasicCapacity)
 {
-    idAllocator.method();
+    ASSERT_EQ(0u, idAllocator.allocate());
+    ASSERT_EQ(1u, idAllocator.allocate());
+    ASSERT_EQ(2u, idAllocator.allocate());
+    ASSERT_EQ(3u, idAllocator.allocate());
+    ASSERT_EQ(4u, idAllocator.allocate());
+    ASSERT_EQ(5u, idAllocator.allocate());
+    ASSERT_EQ(6u, idAllocator.allocate());
+}
+
+TEST_F(IdAllocatorTestSuite, shallReuseIdAfterDeallocation)
+{
+    ASSERT_EQ(0u, idAllocator.allocate());
+    idAllocator.deallocate();
+    ASSERT_EQ(0u, idAllocator.allocate());
+    idAllocator.deallocate();
+}
+
+TEST_F(IdAllocatorTestSuite, shoulNotCrashWhenDeallocatingMoreIdThanAllocated)
+{
+    ASSERT_EQ(0u, idAllocator.allocate());
+    ASSERT_EQ(1u, idAllocator.allocate());
+    idAllocator.deallocate();
+    idAllocator.deallocate();
+    idAllocator.deallocate();
+}
+
+TEST_F(IdAllocatorTestSuite, shallAllocateFromBeginningAfterReset)
+{
+    ASSERT_EQ(0u, idAllocator.allocate());
+    ASSERT_EQ(1u, idAllocator.allocate());
+    ASSERT_EQ(2u, idAllocator.allocate());
+    idAllocator.reset();
+    ASSERT_EQ(0u, idAllocator.allocate());
+}
+
+TEST_F(IdAllocatorTestSuite, shallDeallocateProperlyAfterResizing)
+{
+    ASSERT_EQ(0u, idAllocator.allocate());
+    ASSERT_EQ(1u, idAllocator.allocate());
+    ASSERT_EQ(2u, idAllocator.allocate());
+    ASSERT_EQ(3u, idAllocator.allocate());
+    idAllocator.deallocate();
+    idAllocator.deallocate();
+    idAllocator.deallocate();
+    idAllocator.deallocate();
 }
 
 int main(int argc, char **argv)
